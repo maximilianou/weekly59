5de064eaa7d7a477f8b1eed97631657e00dc0749
maximilianou <maximilianou@gmail.com>
2022-07-15 21:32:16 +0200

    feat(tdd): solidity hardhat tdd

--- blog4/contracts/FunctionIntro.sol
+++ blog4/contracts/FunctionIntro.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+// import "hardhat/console.sol";
+// function log( uint msg ){
+//   console.log( msg );
+// }
+
 contract FunctionIntro {
   function add(uint x, uint y) external pure returns (uint) {
     return x + y;
